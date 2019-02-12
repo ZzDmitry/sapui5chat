847a5e6577e1b14eccc2cd2babff8cd925b82ab3
dmitry <dmitry.zyryanov@execution.su>
2019-02-12 12:08:30 +0700

    refactor

--- server/server.js
+++ server/server.js
@@ -4,14 +4,23 @@ const express = require('express');
 
 const app = express();
 
+function sendClientFile(res, filename) {
+  res.sendFile(
+    filename,
+    {
+      root: '../client/',
+    },
+  );
+}
+
 app.get('/script.js', (req, res) => {
   console.log('get script.js');
-  res.sendFile('script.js', { root: '../client/' });
+  sendClientFile(res, 'script.js');
 });
 
 app.get('/chat.html', (req, res) => {
   console.log('get chat.html');
-  res.sendFile('chat.html', { root: '../client/' });
+  sendClientFile(res, 'chat.html');
 });
 
 app.listen(8001, () => {
